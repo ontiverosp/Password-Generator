d586b61de2f4b440abc952685cd453e231d3398b
Pablo Ontiveros <pablacas@gmail.com>
2020-06-18 16:28:45 -0500

    gets password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passLength;
+var passLower;
+var passUpper;
+var passNum;
+var passSpcl;
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(event) {
+  event.preventDefault();
+  // var password = generatePassword();
+  // Asks for the criteria for pssword
   var passwordText = document.querySelector("#password");
+  passLength = prompt("How many characters should your password be?");
+  passLower = confirm("Should it include lower case letters?");
+  passUpper = confirm("Should it include upper case letters?");
+  passNum = confirm("Should it include numbers?");
+  passSpcl = confirm("Should it include special characters?");
 
-  passwordText.value = password;
+  console.log(passLength);
+  console.log(passLower);
+  console.log(passUpper);
+  console.log(passNum);
+  console.log(passSpcl);
+
+  alert("You have chosen a password that is " + passLength + " characters long. Lower case: " + passLower + " Upper case: " + passUpper + " Numbers: " + passNum + " Special characters: " + passSpcl);
+  // passwordText.value = password;
 
 }
 
